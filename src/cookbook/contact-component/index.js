f883496805ef79ab15352c1098ca2a345f189dbf
wasabi <wasabi@wasabis-MacBook-Pro.local>
2023-04-17 22:30:40 +0100

    contact component

--- src/cookbook/contact-component/index.js
+++ src/cookbook/contact-component/index.js
@@ -1,58 +1,123 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
-function RecipeContactForm() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-    const [submitted, setSubmitted] = useState(false);
+function ContactScreen() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+    });
+    const [errors, setErrors] = useState({});
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await axios.post("/api/contact", { name, email, message });
-            setSubmitted(true);
-        } catch (error) {
-            console.error(error);
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const validateForm = () => {
+        let isValid = true;
+        if (formData.name.trim() === "") {
+            setErrors({ ...errors, name: "Name is required" });
+            isValid = false;
+        }
+        if (formData.email.trim() === "") {
+            setErrors({ ...errors, email: "Email is required" });
+            isValid = false;
+        }
+        if (formData.subject.trim() === "") {
+            setErrors({ ...errors, subject: "Subject is required" });
+            isValid = false;
+        }
+        if (formData.message.trim() === "") {
+            setErrors({ ...errors, message: "Message is required" });
+            isValid = false;
+        }
+        return isValid;
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const isValid = validateForm();
+        if (isValid) {
+            // Send the form data to the backend or an email address
+            console.log(formData);
+            alert("Your message has been sent!");
+            setFormData({
+                name: "",
+                email: "",
+                subject: "",
+                message: "",
+            });
         }
     };
 
     return (
-        <div>
-            {!submitted ? (
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="name">Name:</label>
-                    <input
-                        id="name"
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        id="email"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                    <label htmlFor="message">Message:</label>
-                    <textarea
-                        id="message"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        required
-                    ></textarea>
-                    <button type="submit">Submit</button>
-                </form>
-            ) : (
-                <p>Thank you for contacting us!</p>
-            )}
-        </div>
-    );
-}
+        <Container>
+            <Row className="vh-100 d-flex justify-content-center align-items-center">
+                <Col xs={12} md={6}>
+                    <h1 className="text-center">Contact Us</h1>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="formName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="name"
+                                value={formData.name}
+                                onChange={handleInputChange}
+                                isInvalid={!!errors.name}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.name}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-export default RecipeContactForm;
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleInputChange}
+                                isInvalid={!!errors.email}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.email}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
+                        <Form.Group controlId="formSubject">
+                            <Form.Label>Subject</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="subject"
+                                value={formData.subject}
+                                onChange={handleInputChange}
+                                isInvalid={!!errors.subject}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.subject}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
+                        <Form.Group controlId="formMessage">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={5}
+                                name="message"
+                                value={formData.message}
+                                onChange={handleInputChange}
+                                isInvalid={!!errors.message}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.message}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+            </
